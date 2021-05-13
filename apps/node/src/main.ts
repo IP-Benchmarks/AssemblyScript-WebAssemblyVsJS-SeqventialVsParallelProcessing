0e739bd61aa86a54c03713b6f36ae93c4f83a2a5
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-05-13 15:18:15 +0200

    Patched Worker in the global object for nodeJs - as webpack 4 does not have an implementation for node worker (webpack 5 can use them)

--- apps/node/src/main.ts
+++ apps/node/src/main.ts
@@ -1,3 +1,7 @@
 import { runAllMetrics } from '@ip/benchmark';
+import { NodeWorker } from '@ip/benchmark/glue/worker-implementation';
+
+//@ts-ignore
+globalThis.Worker = NodeWorker;
 
 runAllMetrics();
