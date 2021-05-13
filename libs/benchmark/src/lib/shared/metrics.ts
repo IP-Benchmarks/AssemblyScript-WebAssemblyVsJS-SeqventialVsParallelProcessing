87a61860411c6204c3c5a657dab4903e50500fc8
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-05-14 01:09:46 +0200

    Metrics to Markdown

--- libs/benchmark/src/lib/shared/metrics.ts
+++ libs/benchmark/src/lib/shared/metrics.ts
@@ -15,3 +15,46 @@ export class Metrics implements IMetrics {
         return (timer().now() - (this.startTime ? this.startTime : 0)).toFixed(3);
     }
 }
+
+export function metricsToMarkdownTable(metrics: IMetrics[]) {
+    console.log(metrics);
+    function createHeader(metric: IMetrics) {
+        const loadTime = Array.from(metric.loadTime.keys())
+            .map((x) => ` ${x} (ms) |`)
+            .join('');
+        const computingTime = Array.from(metric.computingTime.keys())
+            .map((x) => ` ${x} (ms) |`)
+            .join('');
+
+        const loadTimeSlots = Array.from(metric.loadTime.keys())
+            .map(() => ' - |')
+            .join('');
+        const computingTimeSlots = Array.from(metric.computingTime.keys())
+            .map(() => ' - |')
+            .join('');
+
+        return `
+        | Amount of numbers | ${loadTime} ${computingTime}
+        | - | ${loadTimeSlots} ${computingTimeSlots}
+        `;
+    }
+
+    function createBody(metrics: IMetrics[]) {
+        const loadTime = (metric: IMetrics) =>
+            Array.from(metric.loadTime.values())
+                .map((x) => ` ${x} |`)
+                .join('');
+        const computingTime = (metric: IMetrics) =>
+            Array.from(metric.computingTime.values())
+                .map((x) => ` ${x} |`)
+                .join('');
+        return metrics
+            .map(
+                (metric) => `| ${metric.arrayLength} | ${loadTime(metric)} ${computingTime(metric)}
+        `
+            )
+            .join('');
+    }
+    if (metrics.length === 0) return '';
+    return createHeader(metrics[0]) + createBody(metrics);
+}
