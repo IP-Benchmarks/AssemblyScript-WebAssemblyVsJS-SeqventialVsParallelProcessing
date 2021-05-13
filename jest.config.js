b315f41c742005f5dae0f54041bc1a3715d62865
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-05-13 16:32:31 +0200

    Removed angular app

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    projects: [
-        '<rootDir>/apps/frontend',
-        '<rootDir>/apps/api',
-        '<rootDir>/libs/benchmark',
-        '<rootDir>/apps/node',
-        '<rootDir>/libs/wasm-generated-js',
-        '<rootDir>/apps/react',
-    ],
+    projects: [, '<rootDir>/apps/api', '<rootDir>/libs/benchmark', '<rootDir>/apps/node', '<rootDir>/libs/wasm-generated-js', '<rootDir>/apps/react'],
 };
